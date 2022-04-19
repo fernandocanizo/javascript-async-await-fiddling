544e93fd4160d3fc272086073dbd671a656ee40a
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-04-19 17:12:12 -0400

    Add level0 module
    Level0 module calls no other modules. It'll be called by others. All
    functions return rejected promises to test stack traces with different
    code patterns

---
+++ level0.js
@@ -0,0 +1,51 @@
+const noAsyncAwait0 = () => Promise.reject(new Error('Something failed'));
+const noAsyncAwait1 = () => Promise.reject(new Error('Something failed'));
+const noAsyncAwait2 = () => Promise.reject(new Error('Something failed'));
+
+const asyncAwaitWithoutTry0 = async () =>
+  await Promise.reject(new Error('Something failed'));
+
+const asyncAwaitWithoutTry1 = async () =>
+  await Promise.reject(new Error('Something failed'));
+
+const asyncAwaitWithoutTry2 = async () =>
+  await Promise.reject(new Error('Something failed'));
+
+const asyncAwaitWithTry0 = async () => {
+  try {
+    await Promise.reject(new Error('Something failed'));
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+const asyncAwaitWithTry1 = async () => {
+  try {
+    await Promise.reject(new Error('Something failed'));
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+const asyncAwaitWithTry2 = async () => {
+  try {
+    await Promise.reject(new Error('Something failed'));
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+module.exports = {
+  noAsyncAwait0,
+  noAsyncAwait1,
+  noAsyncAwait2,
+  asyncAwaitWithoutTry0,
+  asyncAwaitWithoutTry1,
+  asyncAwaitWithoutTry2,
+  asyncAwaitWithTry0,
+  asyncAwaitWithTry1,
+  asyncAwaitWithTry2,
+};
